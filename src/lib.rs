41a51bdd70e54226c83d9caf5e5177d70bc9e0de
Martijn <martijn@tweedegolf.nl>
2019-10-25 11:16:41 +0200

    add example

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+//! # Lorem ispum cratus test amet
+//!
+//!This table has no space between the `//!` and the table start character `|`:
+//!
+//!| a | s | d | f | g |
+//!|---|---|---|---|---|
+//!| h | j | k | l | ; |
+//!
+//! This table has space between the `//!` and the table start character `|`:
+//!
+//! | a | s | d | f | g |
+//! |---|---|---|---|---|
+//! | h | j | k | l | ; |
+//!
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
